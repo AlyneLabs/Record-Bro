e741bed4588b80e7feeb3229afe1e5c78cd05b1a
jaisai1709-afk <jaisai1709@gmail.com>
2026-01-13 21:19:29 +0530

    Home page UI(non-functional) designed and registration tweeks - JK

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/login')
@@ -13,6 +13,10 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
